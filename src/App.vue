70fb7c3e595b39f15f1178b4bd5e66067a70792b
Dylan <dylngae@outlook.com>
2019-12-06 16:50:15 +0100

    add oldList

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
           <v-alert type="success" v-if="addSuccess">Tache ajouté avec succes</v-alert>
           <v-alert type="info" v-if="removeSuccess">Tache supprimé avec succes</v-alert>
           <v-alert type="warning" v-if="confirmSuccess">Tache terminé !</v-alert>
-
         </v-col>
       </v-row>
 
@@ -18,6 +17,9 @@
                 <tr>
                   <th>Tâches en cours</th>
                   <th class="text-right">
+                    <v-btn small color="warning" class="mr-2" @click="enableAddBloc">
+                      <i class="fas fa-ellipsis-h"></i>
+                    </v-btn>
                     <v-btn small color="primary" @click="enableAddBloc">
                       <i class="fa fa-plus"></i>
                     </v-btn>
@@ -60,10 +62,34 @@
         </v-col>
       </v-row>
 
+      <v-card>
+        <v-simple-table>
+          <thead>
+            <tr>
+              <th>Tâches en cours</th>
+            </tr>
+          </thead>
+          <tbody v-if="oldListSuccess">
+            <tr v-for="el in oldList" :key="el">
+              <td class="text-left">{{ e }}</td>
+              <td class="text-right">
+                <v-btn small color="success" class="mr-2" @click="confirmTache(e)">
+                  <i class="fa fa-check"></i>
+                </v-btn>
+                <v-btn small color="warning" class="mr-2">
+                  <i class="fa fa-cog"></i>
+                </v-btn>
+                <v-btn small color="error" @click="removeTache(e)">
+                  <i class="fas fa-trash"></i>
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card>
+
       <v-row class="text-center">
-        <v-col>
-          ToDoList - PiersantiDylan
-        </v-col>
+        <v-col>ToDoList - PiersantiDylan</v-col>
       </v-row>
     </v-container>
   </v-app>
@@ -74,44 +100,42 @@ export default {
   name: "App",
 
   data: () => ({
-    list: [
-    ],
-    oldList: [
-
-    ],
+    list: [],
+    oldList: [],
     show: false,
     addSuccess: false,
     removeSuccess: false,
-    confirmSuccess: false
+    confirmSuccess: false,
+    oldListSuccess: false
   }),
 
   methods: {
-    enableAddBloc: function () {
-      this.show = true
+    enableAddBloc: function() {
+      this.show = true;
     },
 
-    disableAddBloc: function () {
-      this.show = false
+    disableAddBloc: function() {
+      this.show = false;
     },
 
-    addTache: function () {
-      let newTache = document.getElementById('setTache').value
-      this.list.push(newTache)
-      this.addSuccess = true
-      this.removeSuccess = false
+    addTache: function() {
+      let newTache = document.getElementById("setTache").value;
+      this.list.push(newTache);
+      this.addSuccess = true;
+      this.removeSuccess = false;
     },
 
-    removeTache: function (e) {
-      this.list.splice(e, 1)
-      this.removeSuccess = true
-      this.addSuccess = false
+    removeTache: function(e) {
+      this.list.splice(e, 1);
+      this.removeSuccess = true;
+      this.addSuccess = false;
     },
 
-    confirmTache: function (e) {
-      this.oldList.push(this.list[e])
-      this.list.splice(e, 1)
-      this.confirmSuccess = true
-      this.addSuccess = false
+    confirmTache: function(e) {
+      this.oldList.push(this.list[e]);
+      this.list.splice(e, 1);
+      this.confirmSuccess = true;
+      this.addSuccess = false;
     }
   }
 };
